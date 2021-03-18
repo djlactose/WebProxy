fcdbbffd6c50304d231ec08b3f39e3d31d377adc
Nick Hernandez <nick.hernandez@zydoc.com>
2021-03-18 15:40:10 -0400

    Set cert creation first so that restarting nginx would not produce an error caused by the missing certs.

--- add.sh
+++ add.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 read -p "Enter External URL: " extURL
 read -p "Enter Internal URL: " intURL
+certbot certonly --webroot --agree-tos --email $email -n -d $extURL -w /usr/share/nginx/html/
 cp /etc/nginx/conf.d/template.sample_conf /etc/nginx/conf.d/$extURL.conf
 sed -i s/{{extURL}}/$extURL/g /etc/nginx/conf.d/$extURL.conf
 sed -i s/{{intURL}}/$intURL/g /etc/nginx/conf.d/$extURL.conf
-certbot certonly --webroot --agree-tos --email $email -n -d $extURL -w /usr/share/nginx/html/
+nginx -s reload
